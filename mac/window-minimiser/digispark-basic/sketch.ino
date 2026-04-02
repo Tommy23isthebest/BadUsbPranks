6b431492c7594dacafe331b1bb9df23cf7e961dc
Tommy23isthebest <thomas.ciesla@icloud.com>
2026-04-02 18:00:38 +1100

    Create sketch.ino

---
+++ mac/window-minimiser/digispark-basic/sketch.ino
@@ -0,0 +1,10 @@
+#include <DigiKeyboard.h>
+
+void setup() {
+  DigiKeyboard.delay(5000);
+}
+
+void loop() {
+  DigiKeyboard.delay(300000); // every 5 minutes
+  DigiKeyboard.sendKeyStroke(0x51, MOD_GUI_LEFT); // Cmd+M
+}
